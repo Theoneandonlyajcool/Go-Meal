41f059932a3b81eac2bd56500fff14b588ba4df0
Theoneandonlyajcool <ac1106508@gmail.com>
2025-09-29 01:10:18 +0100

    Implented Validation on the signup Page

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,10 +7,112 @@ import { GrMenu } from "react-icons/gr";
 import { IoIosArrowRoundBack } from "react-icons/io";
 
 const Header = () => {
+  const [InputValues, SetInputValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phNumber: "",
+    psw: "",
+    confirmPsw: "",
+  });
+
+  const [ErrorMessages, SetErrorMessages] = useState({
+    FirstNameError: "",
+    LastNameError: "",
+    EmailError: "",
+    PhNumberError: "",
+    PswError: "",
+    ConfirmPswError: "",
+  });
+
   const [MobileMenu, SetMobileMenu] = useState(false);
   const [CartItems, SetCartItems] = useState([]);
   const [DisplayCartItems, SetDisplayCartItems] = useState(false);
   const [SignUp, SetSignUp] = useState(true);
+
+  const Validation = () => {
+    // if (InputValues.firstName == "") {
+    //   SetErrorMessages({
+    //     ...ErrorMessages,
+    //     FirstNameError: "Input First name",
+    //   });
+
+    //   setTimeout(() => {
+    //     SetErrorMessages({ ...ErrorMessages, FirstNameError: "" });
+    //   }, 3000);
+    // }
+
+    // if (InputValues.lastName == "") {
+    //   SetErrorMessages({ ...ErrorMessages, LastNameError: "Input Last name" });
+    //   setTimeout(() => {
+    //     SetErrorMessages({ ...ErrorMessages, LastNameError: "" });
+    //   }, 3000);
+    // }
+
+    let newErrors = {};
+
+    if (InputValues.firstName == "") {
+      newErrors.FirstNameError = "Input your first name";
+    }
+
+    if (InputValues.lastName == "") {
+      newErrors.LastNameError = "Input your last name";
+    }
+
+    if (InputValues.email == "") {
+      newErrors.EmailError = "Pls input your email";
+    } else if (!InputValues.email.includes("@gmail.com")) {
+      newErrors.EmailError = "Invalid email address";
+    }
+
+    if (InputValues.phNumber == "") {
+      newErrors.PhNumberError = "Phone number is required";
+    } else if (InputValues.phNumber.length < 11) {
+      newErrors.PhNumberError = "Phone number must be 11 digits";
+    } else if (InputValues.phNumber.length > 11) {
+      newErrors.PhNumberError = "Phone number must be exactly 11 digits";
+    }
+
+    if (InputValues.psw == "") {
+      newErrors.PswError = "Password is required";
+    } else if (InputValues.psw.length < 8) {
+      newErrors.PswError = "Password must be 8 characters long";
+    } else if (!/[a-z]/.test(InputValues.psw)) {
+      newErrors.PswError =
+        "Password must contain at least one lowercase letter";
+    } else if (!/[A-Z]/.test(InputValues.psw)) {
+      newErrors.PswError =
+        "Password must contain at least one uppercase letter";
+    } else if (!/[0-9]/.test(InputValues.psw)) {
+      newErrors.PswError = "Password must contain at least one number";
+    }
+
+    if (InputValues.confirmPsw == "") {
+      newErrors.ConfirmPswError = "Confirm password is required";
+    } else if (InputValues.confirmPsw !== InputValues.psw) {
+      newErrors.ConfirmPswError = "Passwords don't match";
+    }
+
+    SetErrorMessages(newErrors);
+
+    setTimeout(() => {
+      SetErrorMessages({});
+    }, 4000);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+    // console.log("Form Submitted");
+
+    if (Validation()) {
+      console.log("Form submitted succesfully");
+      console.log(InputValues);
+      SetSignUp(false);
+    } else {
+      console.log("An error occured");
+    }
+  };
 
   return (
     <div className="Header_cont">
@@ -159,21 +261,215 @@ const Header = () => {
                   <form
                     className="sign_up_input_form"
                     action="submit"
-                    onSubmit={(e) => e.preventDefault()}
+                    onSubmit={HandleSubmit}
                   >
                     {/* name */}
-                    <div className="sign_up_name_input_holder">
-                      <div>
+                    <div
+                      className="sign_up_name_input_holder"
+                      style={{ width: "100%" }}
+                    >
+                      {/* First name */}
+                      <div
+                        style={{
+                          width: "50%",
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "center",
+                        }}
+                      >
                         <label htmlFor="">First name</label>
 
-                        <input type="text" />
+                        <input
+                          type="text"
+                          className="sign_up_inputs"
+                          // required
+                          value={InputValues.firstName}
+                          onChange={(e) =>
+                            SetInputValues({
+                              ...InputValues,
+                              firstName: e.target.value,
+                            })
+                          }
+                        />
+                        <p
+                          style={{
+                            height: ".5rem",
+                            color: "red",
+                            alignSelf: "center",
+                          }}
+                        >
+                          {ErrorMessages.FirstNameError}
+                        </p>
                       </div>
 
-                      <div>
+                      {/* last name */}
+                      <div
+                        style={{
+                          width: "50%",
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "center",
+                          // textAlign: "center",
+                        }}
+                      >
                         <label htmlFor="">Last name</label>
-                        <input type="text" />
+                        <input
+                          type="text"
+                          className="sign_up_inputs"
+                          // required
+                          value={InputValues.lastName}
+                          onChange={(e) =>
+                            SetInputValues({
+                              ...InputValues,
+                              lastName: e.target.value,
+                            })
+                          }
+                        />{" "}
+                        <p
+                          style={{
+                            height: ".5rem",
+                            color: "red",
+                            alignSelf: "center",
+                          }}
+                        >
+                          {ErrorMessages.LastNameError}
+                        </p>
                       </div>
                     </div>
+
+                    {/* Email input */}
+                    <div
+                      style={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                      }}
+                    >
+                      <label htmlFor="email">Email</label>
+                      <input
+                        type="email"
+                        name="email"
+                        className="sign_up_inputs"
+                        // required
+                        value={InputValues.email}
+                        onChange={(e) =>
+                          SetInputValues({
+                            ...InputValues,
+                            email: e.target.value,
+                          })
+                        }
+                      />
+                      <p
+                        style={{
+                          height: ".5rem",
+                          color: "red",
+                          alignSelf: "center",
+                        }}
+                      >
+                        {ErrorMessages.EmailError}
+                      </p>
+                    </div>
+
+                    {/* phone number input */}
+                    <div
+                      style={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                      }}
+                    >
+                      <label htmlFor="ph_numbet">Phone Number</label>
+                      <input
+                        type="number"
+                        className="sign_up_inputs"
+                        value={InputValues.phNumber}
+                        onChange={(e) =>
+                          SetInputValues({
+                            ...InputValues,
+                            phNumber: e.target.value,
+                          })
+                        }
+                      />
+                      <p
+                        style={{
+                          height: ".5rem",
+                          color: "red",
+                          alignSelf: "center",
+                        }}
+                      >
+                        {ErrorMessages.PhNumberError}
+                      </p>
+                    </div>
+
+                    {/* Password */}
+                    <div
+                      style={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                      }}
+                    >
+                      <label htmlFor="">Password</label>
+                      <input
+                        type="password"
+                        className="sign_up_inputs"
+                        // required
+                        value={InputValues.psw}
+                        onChange={(e) =>
+                          SetInputValues({
+                            ...InputValues,
+                            psw: e.target.value,
+                          })
+                        }
+                      />
+                      <p
+                        style={{
+                          height: ".5rem",
+                          color: "red",
+                          alignSelf: "center",
+                        }}
+                      >
+                        {ErrorMessages.PswError}
+                      </p>
+                    </div>
+
+                    <div
+                      style={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                      }}
+                    >
+                      <label htmlFor="">Confirm password</label>
+                      <input
+                        type="password"
+                        className="sign_up_inputs"
+                        // required
+                        value={InputValues.confirmPsw}
+                        onChange={(e) =>
+                          SetInputValues({
+                            ...InputValues,
+                            confirmPsw: e.target.value,
+                          })
+                        }
+                      />
+                      <p
+                        style={{
+                          height: ".5rem",
+                          color: "red",
+                          alignSelf: "center",
+                        }}
+                      >
+                        {ErrorMessages.ConfirmPswError}
+                      </p>
+                    </div>
+
+                    <button className="sign_up_btn" style={{ width: "100%" }}>
+                      Sign up
+                    </button>
+                    <p>
+                      Already have an account ? <b>Sign in</b>{" "}
+                    </p>
 
                     {/*  */}
                   </form>
